07d3d12ced7d80657962c8da30e88e7f0ca8ce10
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-04-01 22:52:13 +0300

    add simple React elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const el = (
+    <div>
+        <h1>My todo list</h1>
+        <input placeholder="search"/>
+        <ul>
+            <li>Drink coffee</li>
+            <li>Learn React</li>
+            <li>Create an awesome app :)</li>
+        </ul>
+    </div>
+);
+ReactDOM.render(el, document.getElementById('root'));
