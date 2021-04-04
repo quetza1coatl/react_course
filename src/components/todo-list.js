1726731953f4dfcfe73ddd941cfb0a14cc19120a
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-04-04 19:19:40 +0300

    add simple props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,9 +4,9 @@ import TodoListItem from './todo-list-item'
 const TodoList = () =>{
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            <li><TodoListItem label = "Drink Coffee"/></li>
+            <li><TodoListItem label = "Learn React" important/></li>
+            <li><TodoListItem label = "Create an awesome app :)"/></li>
         </ul>
     );
 };
