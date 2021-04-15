1db6e8ec891bc7c380f01c9a797a564f3b9a49be
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-04-15 22:37:50 +0300

    add class component

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends React.Component{
 
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
+    render() {
+        const {label, important = false} = this.props;
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
 
-    return (
-        <span className="todo-list-item">
+        return (
+            <span className="todo-list-item">
       <span
           className="todo-list-item-label"
           style={style}>
@@ -18,15 +20,14 @@ const TodoListItem = ({ label, important = false }) => {
 
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
+        <i className="fa fa-exclamation"/>
       </button>
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
+        <i className="fa fa-trash-o"/>
       </button>
     </span>
-    );
-};
-
-export default TodoListItem;
+        );
+    }
+}
