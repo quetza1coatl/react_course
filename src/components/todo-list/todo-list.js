9015edcf73c136a8eff39ab3d31490f3fd894bc8
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-05-03 23:15:53 +0300

    custom events with callback function

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,7 +2,7 @@ import React from 'react'
 import TodoListItem from '../todo-list-item/todo-list-item'
 import './todo-list.css'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
 
     const elements = todos.map((item) => {
         // extract id,  remaining properties will be put into an array itemProps
@@ -10,7 +10,9 @@ const TodoList = ({ todos }) => {
 
         return (
             <li key={id} className="list-group-item">
-                <TodoListItem {...itemProps } />
+                <TodoListItem
+                    {...itemProps }
+                    onDeleted={() => onDeleted(id)}/>
             </li>
         );
     });
