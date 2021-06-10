861bf3ff6553cc02ac6f372d401d1a7e1a2ad1f6
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-06-10 22:05:08 +0300

    add input for new items

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,16 +3,33 @@ import './item-add-form.css'
 
 export default class ItemAddForm extends React.Component{
 
+	state = {
+		label: ''
+	};
+
+	onLabelChange = (e) => {
+		this.setState({
+			label: e.target.value
+		});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.onItemAdded(this.state.label);
+	};
+
     render(){
         return (
-            <div className="item-add-form">
-        <button type="button"
-                className="btn btn-outline-secondary"
-                onClick={ () => this.props.onItemAdded('New item') } >
-            <i className="fa fa-plus"/>
-             Add item
-        </button>
-            </div>);
+            <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+            <input type="text"
+             className="form-control"
+             onChange={this.onLabelChange}
+             placeholder="What needs to be done"/>
+        <button type="submit"
+                className="btn btn-outline-secondary">
+            <i className="fa fa-plus"/> Add item</button>
+            </form>);
     }
 
 }
