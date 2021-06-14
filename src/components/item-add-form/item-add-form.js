5368a89df3ab6494c6decaa158ec56a27e770003
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-06-14 22:24:14 +0300

    controlled html elements

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -16,6 +16,9 @@ export default class ItemAddForm extends React.Component{
 	onSubmit = (e) => {
 		e.preventDefault();
 		this.props.onItemAdded(this.state.label);
+		this.setState( {
+			label: ''
+		});
 	};
 
     render(){
@@ -25,7 +28,8 @@ export default class ItemAddForm extends React.Component{
             <input type="text"
              className="form-control"
              onChange={this.onLabelChange}
-             placeholder="What needs to be done"/>
+             placeholder="What needs to be done"
+			value={this.state.label}/>
         <button type="submit"
                 className="btn btn-outline-secondary">
             <i className="fa fa-plus"/> Add item</button>
