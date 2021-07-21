e55781b23963753860eb5c712181ef8983d6987d
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-07-21 22:00:11 +0300

    add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends React.Component {
         	this.createTodoItem('Make Awesome App'),
         	this.createTodoItem('Have a lunch')
         ],
-        term: ''
+        term: '',
+        filter: 'all'  // all|active|done
     };
 
     createTodoItem(label){
@@ -91,13 +92,29 @@ export default class App extends React.Component {
         });
     }
 
+    filter(items, filter) {
+        switch (filter) {
+            case 'active':
+                return items.filter( (item) => !item.done);
+            case 'done':
+                return items.filter((item) =>item.done);
+            default:
+                return items;
+        }
+
+    }
+
     onSearchChange = (term) => {
         this.setState({ term });
     };
 
+    onFilterChange = (filter) => {
+        this.setState({ filter });
+    };
+
     render(){
-    	const { todoData, term } = this.state;
-    	const visibleItems = this.search(todoData, term);
+    	const { todoData, term, filter } = this.state;
+    	const visibleItems = this.filter(this.search(todoData, term), filter);
     	const doneCount = todoData
     						.filter((el) => el.done).length;
     	const todoCount = todoData.length - doneCount;
@@ -106,7 +123,8 @@ export default class App extends React.Component {
                 <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex">
                     <SearchPanel onSearchChange={this.onSearchChange}/>
-                    <ItemStatusFilter />
+                    <ItemStatusFilter filter={filter}
+                    onFilterChange={this.onFilterChange}/>
                 </div>
 
                 <TodoList
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -3,15 +3,24 @@ import './item-status-filter.css'
 
 export default class ItemStatusFilter extends React.Component{
 
+    buttons = [
+        {name:"all", label: "All"},
+        {name:"done", label: "Done"},
+        {name:"active", label: "Active"}
+    ];
+
     render(){
+        const { filter, onFilterChange } = this.props;
+        const buttons = this.buttons.map(( {name, label} ) => {
+            const isActive = filter === name;
+            const clazzName = isActive? "btn btn-info" : "btn btn-outline-secondary";
+            return  <button key={name} type="button"
+                            className={clazzName}
+                            onClick={ () => onFilterChange(name)}>{label}</button>
+        });
         return (
             <div className="btn-group">
-                <button type="button"
-                        className="btn btn-info">All</button>
-                <button type="button"
-                        className="btn btn-outline-secondary">Active</button>
-                <button type="button"
-                        className="btn btn-outline-secondary">Done</button>
+                {buttons}
             </div>
         );
     }
